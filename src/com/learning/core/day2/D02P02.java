9a9b683bb6df400e49bc0775b473b3362bcdc6cf
AmitJ021 <161856626+AmitJ021@users.noreply.github.com>
2024-03-08 01:36:36 +0530

    Add files via upload

---
+++ src/com/learning/core/day2/D02P02.java
@@ -0,0 +1,24 @@
+package com.learning.core.day2;
+
+import java.util.Scanner;
+
+public class D02P02 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner ch= new Scanner(System.in);
+		
+		 System.out.println("Enter a number: ");
+		    int num = ch.nextInt();
+
+		    int i = 0;
+		    while (i <= num) {
+		      if (i % 2 == 0) {
+		        System.out.println(i);
+		      }
+		      i++;
+
+	}
+	}
+}
