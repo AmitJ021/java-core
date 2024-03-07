27dbdb1c4898be0dfe54f77fca9e8274ccf5e4a1
AmitJ021 <161856626+AmitJ021@users.noreply.github.com>
2024-03-08 01:43:01 +0530

    Add files via upload

---
+++ src/com/learning/core/day5/D05P03.java
@@ -0,0 +1,42 @@
+package com.learning.core.day5;
+import java.util.HashSet;
+
+class Car
+{
+	private String id;
+	private String name;
+
+	public Car(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return  id + " " + name;
+	}
+	
+}
+	
+public class D05P03 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		HashSet<Car> c = new HashSet<>();
+
+	    c.add(new Car("P001", "Maruti 800"));
+	    c.add(new Car("P002", "Maruti Zen"));
+	    c.add(new Car("P003", "Maruti Dezire"));
+	    c.add(new Car("P004", "Maruti Alto"));
+
+	    for (Car i : c)
+	    {
+	      System.out.println(i);
+	    }
+	   
+	}
+}
